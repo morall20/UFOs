b3b75ba1f2c356fe24303e6b1f40128ec050e135
morall20 <82338072+morall20@users.noreply.github.com>
2021-06-30 14:38:53 -0400

    updated files

--- static/js/app.js
+++ static/js/app.js
@@ -4,11 +4,6 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-}
-
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
